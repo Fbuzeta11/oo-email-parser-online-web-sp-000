3ee0e2baef7e889d3f4c360b5210ef50a3d53dfe
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-01 18:03:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,12 +12,12 @@ class EmailParser
    csv_emails.split(/\s*,\s*/)
  end 
 
-  def parse 
-    csv_emails.split.collect do |address|
-      address.split(',') 
-    end
-    .flatten.uniq 
-  end 
+  #def parse 
+    #csv_emails.split.collect do |address|
+      #address.split(',') 
+    #end
+    #.flatten.uniq 
+  #end 
 end 
 
 emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
