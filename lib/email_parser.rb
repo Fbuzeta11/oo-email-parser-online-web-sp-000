101951dfa5e9efae5cd0c9e9caf9a8241e945641
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-01 18:10:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,12 +1,14 @@
 class EmailParser
-attr_accessor :emails
 
-def initialize(email)
-  @emails = email
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
+  end
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
 end
-
-def parse
-  emails.delete(',').split.uniq
-end
-
-end 
